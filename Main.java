13dcf8e291dbb9bbfed532798e1d719ce5f1d59a
wieslawpaleta <tankluk@gmail.com>
2026-09-07 18:44:01 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,12 @@
+//Java w3schools
+//Introducción 
+//Recuerda que el nombre de la clase es el nombre del archivo
+
+public class Main {
+    public static void main(String[] args) {
+        String nombre = "José";
+        int edad = 100;
+        System.out.println("Hola " + nombre + ", de " + edad + " años!" );
+    }
+}
+//Java Syntax 
