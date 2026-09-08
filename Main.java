e9716dc2fb0a9e81876a955fffc441872b104a85
wieslawpaleta <tankluk@gmail.com>
2026-09-08 18:04:52 +0200

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,12 +1,17 @@
 //Java w3schools
 //Introducción 
-//Recuerda que el nombre de la clase es el nombre del archivo
+/*Java Syntax, Statements, Syntax Code Challenge, Output/Print, Output Numbers, Output Code Challenge, Comments (teraz tu jestem)*/
+/*Recuerda que el nombre de la clase es el nombre del archivo*/
 
 public class Main {
     public static void main(String[] args) {
         String nombre = "José";
         int edad = 100;
         System.out.println("Hola " + nombre + ", de " + edad + " años!" );
+        System.out.println("Java es divertido!");
+        System.out.print("Y eso es lo que me gusta!\n");
+        System.out.println("dos");
+        System.out.println("tres");
+        System.out.println(2 * 3);
     }
-}
-//Java Syntax 
+} 
